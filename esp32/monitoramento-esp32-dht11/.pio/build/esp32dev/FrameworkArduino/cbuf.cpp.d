507869dd464293d56846ca347810285ca24eb064
DevKarolinyBarreto <karolinybarreto65@gmail.com>
2025-05-11 14:55:49 -0300

    Projeto

---
+++ esp32/monitoramento-esp32-dht11/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/karoliny.bmsantos/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/karoliny.bmsantos/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
